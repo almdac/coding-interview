5d6994925d2547778083edc92d11cc2e8f882f8c
Almir Cunha <almirmdacunha@gmail.com>
2021-08-07 21:26:15 -0300

    refact(fibonacci): remove unnecessary return and else statement

--- fibonacci.py
+++ fibonacci.py
@@ -8,6 +8,4 @@ class Solution:
             return n
         if self.memo[n] == 0:
             self.memo[n] = self.fib(n-1) + self.fib(n-2)
-            return self.memo[n]
-        else:
-            return self.memo[n]
+        return self.memo[n]
