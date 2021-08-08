9381adc47bfcd3da5cd9b628c65886f9d2658ffe
Almir Cunha <almirmdacunha@gmail.com>
2021-08-08 14:54:07 -0300

    feat(find-sum-indexes): add solution

---
+++ find_sum_indexes.py
@@ -0,0 +1,13 @@
+# Given an array arr of size n, and a number x, find 2 indexes i, j such that when i != j,
+# arr[i] + arr[j] == x in O(n) time
+
+class Solution:
+    def find_sum_indexes(self, arr: List[int], x: int) -> Tuple[int, int]:
+        hash = {}
+        for i, n in enumerate(arr):
+            if n in hash:
+                return (hash[n], i)
+            else:
+                hash[x-n] = i
+
+        return None
